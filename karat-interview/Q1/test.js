f5c0aba5fff7e6ed927ef8c90dca177e92535045
derekm28 <derekmurray928@gmail.com>
2022-07-25 20:56:28 -0400

    added more tests to Q1

--- karat-interview/Q1/test.js
+++ karat-interview/Q1/test.js
@@ -2,6 +2,22 @@
 
 const { find_rectangle } = require('./submission');
 
+const image1 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 0, 0, 0, 1],
+  [1, 1, 1, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+];
+
+const image2 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 0],
+];
+
 const image3 = [
   [1, 1, 1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1],
@@ -9,6 +25,14 @@ const image3 = [
   [1, 1, 1, 1, 1, 0, 0],
   [1, 1, 1, 1, 1, 0, 0],
 ];
+
+test('find_rectangle - test 1', function () {
+  expect(find_rectangle(image1)).toBe([3, 5]);
+});
+
+test('find_rectangle - test 1', function () {
+  expect(find_rectangle(image2)).toBe([3, 5]);
+});
 
 test('find_rectangle - test 1', function () {
   expect(find_rectangle(image3)).toBe([3, 5]);
